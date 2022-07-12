1ca3c8e921d52fdb87016e68a608aa25c25962ac
SavindyaPrathibhani <savindyaabeysingha@gmail.com>
2022-07-12 11:18:54 +0530

    second Commit

---
+++ src/controller/MainFormController.java
@@ -0,0 +1,25 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+public class MainFormController {
+    public Button btnRegister;
+    public Button btnSearch;
+    public Button btnUpdate;
+    public Button btnDelete;
+    public AnchorPane loader;
+
+    public void registerStudentOnAction(ActionEvent actionEvent) {
+    }
+
+    public void searchOnAction(ActionEvent actionEvent) {
+    }
+
+    public void updateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void deleteOnAction(ActionEvent actionEvent) {
+    }
+}
