7b0745fc87dc2234068259bcdf0f6eb8a629a6a5
SavindyaPrathibhani <savindyaabeysingha@gmail.com>
2022-07-12 12:49:51 +0530

    fifth Commit

--- src/controller/RegisterCrudController.java
+++ src/controller/RegisterCrudController.java
@@ -8,14 +8,14 @@ import java.sql.SQLException;
 
 public class RegisterCrudController {
     public boolean saveStudent(Student student) throws SQLException, ClassNotFoundException {
-        return CrudUtil.execute("INSERT INTO Student VALUES(?,?,?,?,?,?,?)",
+        return CrudUtil.execute("INSERT INTO Student VALUES(?,?,?,?,?,?)",
                 student.getStudent_id(),student.getStudent_name(),student.getEmail(),student.getContact(),student.getAddress(),student.getNic());
 
     }
 
     public boolean saveRegisterDetails(Registration r) throws SQLException, ClassNotFoundException {
 
-        boolean isDetailsSaved= CrudUtil.execute("INSERT INTO Registration VALUES(?,?,?,?,?,?,?)",
+        boolean isDetailsSaved= CrudUtil.execute("INSERT INTO Registration VALUES(?,?,?,?)",
                 r.getRegistration_id(),r.getReg_date(),r.getStudent_id(),r.getIntake_id());
         return isDetailsSaved;
     }
