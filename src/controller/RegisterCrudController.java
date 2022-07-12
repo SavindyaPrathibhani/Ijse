120154f862d7350e8326106e287386741053afa8
SavindyaPrathibhani <savindyaabeysingha@gmail.com>
2022-07-12 12:33:07 +0530

    fifth Commit

--- src/controller/RegisterCrudController.java
+++ src/controller/RegisterCrudController.java
@@ -1,4 +1,22 @@
 package controller;
 
+import model.Registration;
+import model.Student;
+import util.CrudUtil;
+
+import java.sql.SQLException;
+
 public class RegisterCrudController {
+    public boolean saveStudent(Student student) throws SQLException, ClassNotFoundException {
+        return CrudUtil.execute("INSERT INTO Student VALUES(?,?,?,?,?,?,?)",
+                student.getStudent_id(),student.getStudent_name(),student.getEmail(),student.getContact(),student.getAddress(),student.getNic());
+
+    }
+
+    public boolean saveRegisterDetails(Registration r) throws SQLException, ClassNotFoundException {
+
+        boolean isDetailsSaved= CrudUtil.execute("INSERT INTO Registration VALUES(?,?,?,?,?,?,?)",
+                r.getRegistration_id(),r.getReg_date(),r.getStudent_id(),r.getIntake_id());
+        return isDetailsSaved;
+    }
 }
--- src/controller/RegisterFormController.java
+++ src/controller/RegisterFormController.java
@@ -1,8 +1,16 @@
 package controller;
 
+import db.DbConnection;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import model.Registration;
+import model.Student;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class RegisterFormController {
     public TextField txtID;
@@ -16,6 +24,56 @@ public class RegisterFormController {
     public TextField txtIntakeId;
     public Button btnRegister;
 
-    public void registerOnAction(ActionEvent actionEvent) {
+    public void registerOnAction(ActionEvent actionEvent) throws SQLException {
+
+
+
+        Student student = new Student(
+                txtID.getText(),
+                txtName.getText(),
+                txtEmail.getText(),
+                txtContact.getText(),
+                txtAddress.getText(),
+                txtNic.getText()
+
+        );
+
+        ArrayList<Registration> reg_details = new ArrayList<>();
+
+        Registration r = new Registration(
+               txtRegID.getText(),
+                txtRegDate.getText(),
+                txtID.getText(),
+                txtIntakeId.getText()
+        );
+        Connection connection= null;
+
+        try {
+            connection= DbConnection.getInstance().getConnection();
+            connection.setAutoCommit(false);
+            boolean isStudentSaved = new RegisterCrudController().saveStudent(student);
+            if (isStudentSaved) {
+                boolean isRegisterDetailsSaved=new RegisterCrudController().saveRegisterDetails(r);
+                if (isRegisterDetailsSaved){
+                    connection.commit();
+                    new Alert(Alert.AlertType.CONFIRMATION,"Saved!").show();
+                }else{
+                    connection.rollback();
+                    new Alert(Alert.AlertType.ERROR,"Error!").show();
+                }
+            }else{
+                connection.rollback();
+                new Alert(Alert.AlertType.ERROR,"Error!").show();
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println(e);
+        }finally {
+            connection.setAutoCommit(true);
+        }
+
+        clearAllTextFields();
+    }
+
+    private void clearAllTextFields() {
     }
 }
